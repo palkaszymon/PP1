c37a44e0b5da204e2dc9c5c04b17468edb4cae44
szymonpalka1 <91990030+szymonpalka1@users.noreply.github.com>
2021-10-20 09:28:27 +0200

    Add files via upload

---
+++ ControlStructures/czy liczba jest parzysta.py
@@ -0,0 +1,3 @@
+x=int(input("Podaj liczbę: "))
+if (x % 2) == 0:
+    print(f"Liczba {x} jest parzysta")
---
+++ ControlStructures/sprawdzanie wieku.py
@@ -0,0 +1,5 @@
+adult_age=18
+annie_age=18
+paul_age=21
+if annie_age >= adult_age and paul_age >= adult_age:
+    print("Both are adults")
